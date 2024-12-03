a512961ba4d8eae74e57ebfa837d81f45128ca96
MartinMartinov <martinovm@gmail.com>
2024-12-03 17:53:29 -0500

    Tweaks and bugfixes to alarm task

--- src/Automated_Tasker/tasks/alarm.py
+++ src/Automated_Tasker/tasks/alarm.py
@@ -32,37 +32,34 @@ class SetAlarm:
         """
         calendar = GoogleCalendarClient(vault)
         for event in calendar.get_todays_events():
+            if not event['summary'].startswith('-w'):
+                continue
+
+            alarm_time = timedelta(minutes=30)
             if "overrides" in event["reminders"]:
                 offset = timedelta(minutes=event["reminders"]["overrides"][0]["minutes"])
                 etime = datetime.strptime(event["start"]["dateTime"][:19], "%Y-%m-%dT%H:%M:%S") - offset
                 alarm_time = timedelta(hours=etime.hour, minutes=etime.minute, seconds=etime.second)
 
-                class Alarm:
-                    """An etheral task created for setting off an alarm at a variable time."""
+            class Alarm:
+                """An etheral task created for setting off an alarm at a variable time."""
 
-                    NAME: str = "Alarm"
-                    TIME: timedelta = alarm_time
-                    DAYS: List[str] = []
-                    DAY: int = 0
+                NAME: str = "Alarm"
+                TIME: timedelta = alarm_time
+                DAYS: List[str] = []
+                DAY: int = 0
 
-                    async def execute(self, vault: Vault | None = None):
-                        """Start all the SwitchBot alarm devices.
+                async def execute(self, vault: Vault | None = None):
+                    """Start all the SwitchBot alarm devices.
 
-                        Parameters:
-                            vault (Vault | None): The vault with the switchbot token and secret
-                        """
-                        tokens = vault.load_entries()
-                        controller = SwitchBotController(tokens["switchbot-token"], tokens["switchbot-secret"])
-                        #tasks = []
-                        async with ClientSession() as session:
-                            #await controller.fetch_devices(session)
-                            #tasks.append(controller.open_curtains(session))
-                            #tasks.append(controller.turn_on_light_bulbs(session))
-                            #tasks.append(controller.turn_on_plug_alarms(session))
-                            await controller.fetch_scenes(session)
-                            await controller.activate_scene(session, controller.lookup_scene("Alarm Start"))
-                            #await asyncio.gather(*tasks)
+                    Parameters:
+                        vault (Vault | None): The vault with the switchbot token and secret
+                    """
+                    tokens = vault.load_entries()
+                    controller = SwitchBotController(tokens["switchbot-token"], tokens["switchbot-secret"])
+                    async with ClientSession() as session:
+                        await controller.fetch_scenes(session)
+                        await controller.activate_scene(session, controller.lookup_scene("Alarm Start"))
 
-                alarm = Alarm()
-                Tasks.add_daily_tasklist(Alarm())
-                logger.info(f"Added {alarm.NAME} ({alarm.TIME}) to daily tasklist.")
+            Tasks.add_daily_tasklist(Alarm())
+            logger.info(f"Added Alarm ({alarm_time}) to daily tasklist.")
